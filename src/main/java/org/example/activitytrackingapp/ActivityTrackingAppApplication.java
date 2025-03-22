7039e2201bd7687b6e1bb668c6c395e9b72a6644
JADDOU3 <22the7@gmail.com>
2025-03-22 06:58:11 +0200

    implementing activity scheduler

--- src/main/java/org/example/activitytrackingapp/ActivityTrackingAppApplication.java
+++ src/main/java/org/example/activitytrackingapp/ActivityTrackingAppApplication.java
@@ -2,8 +2,11 @@ package org.example.activitytrackingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class ActivityTrackingAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/activitytrackingapp/scheduler/ActivityScheduler.java
+++ src/main/java/org/example/activitytrackingapp/scheduler/ActivityScheduler.java
@@ -1,6 +1,19 @@
 package org.example.activitytrackingapp.scheduler;
 
 
+import lombok.RequiredArgsConstructor;
+import org.example.activitytrackingapp.services.ActivityService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class ActivityScheduler {
+
+    private final ActivityService activityService;
+
+    @Scheduled(fixedRate = 60000) // every minute
+    public void track() {
+        activityService.checkActivity();
+    }
 }
